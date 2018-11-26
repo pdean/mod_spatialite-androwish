39d3df04175f3898fe074c54835aa62de1a3ad64
Peter Dean <p.dean@internode.on.net>
2018-11-27 07:08:40 +1000

    Update downloads.sh

--- jni/spatialite/downloads.sh
+++ jni/spatialite/downloads.sh
@@ -3,7 +3,7 @@ export BUILD=x86_64-pc-linux-gnu
 
 #proj
 ver=5.2.0
-#wget http://download.osgeo.org/proj/proj-${ver}.tar.gz
+wget http://download.osgeo.org/proj/proj-${ver}.tar.gz
 tar -xvzf proj-${ver}.tar.gz
 cd proj-${ver}/
 # versions in tar too old for android
@@ -15,7 +15,7 @@ cd ..
 
 #sqlite
 ver=3250300
-#wget http://www.sqlite.org/2018/sqlite-autoconf-${ver}.tar.gz
+wget http://www.sqlite.org/2018/sqlite-autoconf-${ver}.tar.gz
 tar -xvzf sqlite-autoconf-${ver}.tar.gz
 cd sqlite-autoconf-${ver}/
 # versions in tar too old for android
@@ -27,7 +27,7 @@ cd ..
 
 #geos
 ver=3.6.3
-#wget  http://download.osgeo.org/geos/geos-${ver}.tar.bz2
+wget  http://download.osgeo.org/geos/geos-${ver}.tar.bz2
 tar -xvjf geos-${ver}.tar.bz2 
 cd geos-${ver}/
 # versions in tar too old for android
@@ -39,7 +39,7 @@ cd ..
 
 #libspatialite
 ver=5.0.0-beta0
-#wget http://www.gaia-gis.it/gaia-sins/libspatialite-sources/libspatialite-${ver}.tar.gz
+wget http://www.gaia-gis.it/gaia-sins/libspatialite-sources/libspatialite-${ver}.tar.gz
 tar -xvzf libspatialite-${ver}.tar.gz
 cd libspatialite-${ver}/
 # versions in tar too old for android
